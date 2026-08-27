a0fd7e7c49d33891a73511753a2283416324444e
rte26 <rte26@uclive.ac.nz>
2026-08-27 12:33:08 +1200

    priority-queue.js has now been ported , all tests passing

--- src/util/priority-queue.ts
+++ src/util/priority-queue.ts
@@ -6,37 +6,47 @@
 // a profiling and refactoring tidyup.
 //
 // @maetl / 2017
-class PriorityQueue {
+class PriorityQueue <QueueElementType> {
+
+  private _heap : ([number,QueueElementType]|null)[];
+  private _size : number;
+
   constructor() {
     this._heap = [];
     this._size = 0;
   }
 
-  enqueue(item, priority) {
+  enqueue(item : QueueElementType , priority : number) {
     this._heap[++this._size] = [priority, item];
     this.bubbleUp(this._size);
   }
 
-  dequeue() {
-    var first = this._heap[1][1];
+  dequeue() : QueueElementType | undefined {
+
+    // I (ryan) added this behaviour , previously deque from an empty q would just crash
+    if ( this._heap[1] === null || this._heap[1] === undefined )
+    {
+      return undefined;
+    }
+    const first = this._heap[1][1];
     this.swap(1, this._size--);
     this._heap[this._size + 1] = null;
     this.bubbleDown(1);
     return first;
   }
 
-  isEmpty() {
-    return this._size == 0;
+  isEmpty() : boolean {
+    return this._size === 0;
   }
 
-  bubbleUp(pos) {
+  bubbleUp(pos : number) {
     while (pos > 1 && this.comparison(Math.floor(pos / 2), pos)) {
       this.swap(Math.floor(pos / 2), pos);
       pos = Math.floor(pos / 2);
     }
   }
 
-  bubbleDown(pos) {
+  bubbleDown(pos : number) {
     while (2 * pos <= this._size) {
       let next = 2 * pos;
       if (next < this._size && this.comparison(next, next + 1)) next++;
@@ -46,11 +56,19 @@ class PriorityQueue {
     }
   }
 
-  comparison(a, b) {
+  comparison(a : number , b : number) : boolean{
+
+    // behaviour added by me (ryan) , likely overly defensive
+    if (this._heap[a] === null || this._heap[a] === undefined ||
+        this._heap[b] === null || this._heap[b] === undefined) 
+    {
+      return false;
+    }
+
     return this._heap[a][0] > this._heap[b][0];
   }
 
-  swap(a, b) {
+  swap(a : number, b : number) {
     const item = this._heap[a];
     this._heap[a] = this._heap[b];
     this._heap[b] = item;
