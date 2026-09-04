07b025d5819b70ffcb1001fa336d3da0d09141f1
Ryan Teape <rte26@uclive.ac.nz>
2026-09-04 19:20:34 +1200

    have added types to node.ts but now some test cases fail which expect an undefined when accessing props that dont exist (node.title) , need to try to figure out or/and ask mark if the traversal depends on this returns undefined behaviour

--- src/node.ts
+++ src/node.ts
@@ -29,29 +29,3 @@ class Node {
 }
 
 export default Node;
-
-// class Node {
-
-//   nodePropVals : number | string | boolean;
-//   nodeProps : Record< string ,  >
-
-//   type nodeP
-
-//   constructor({ id, label, props } = {}) {
-//     this.id = id;
-//     this.label = label;
-//     this.props = props || {};
-
-//     for (let key of Object.keys(this.props)) {
-//       Object.defineProperty(this, key, {
-//         enumerable: true,
-//         writable: false,
-//         value: props[key]
-//       });
-//     }
-
-//     Object.preventExtensions(this);
-//   }
-// }
-
-// export default Node;
