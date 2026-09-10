c101003af3c944aa78986c4b4dab33c0829959c6
Ryan Teape <rte26@uclive.ac.nz>
2026-09-10 14:21:33 +1200

    edge.js ported , compiling and passing tests

--- src/example-query.ts
+++ src/example-query.ts
@@ -1,6 +1,6 @@
 // import Graph from 'src/graph.js';
 // import Node from './node.ts';
-// import Edge from './edge.js';
+import Edge from './edge.ts';
 
 // const graph = new Graph(g => {
 //   const alice = new Node({ id: 1, label: 'person', props: { name: 'Age List', age: [30,20,11,22,33] } });
@@ -21,6 +21,7 @@
 // const node4 = new Node({ id: "invalid", label: 'person', props: { name: 'Bob', age: 25 } });
 
 
+
 // console.log('All people:');
 // console.log(graph.n('person').all().map(n => n.props.name));
 
@@ -32,4 +33,6 @@
 // const adults = graph.n('person').filter(n => n.props.age > 26).all();
 // console.log(adults.map(n => `${n.props.name} (${n.props.age})`));
 
+const testEdge = new Edge({from:6, to:3, label: "77", props: {"exampelString" : [222,12,122,33]} });
 
+console.log(testEdge)
